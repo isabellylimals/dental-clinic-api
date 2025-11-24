5da217f6a3c14e844ce14f5861dd0b9f4cc8e378
kayquelopes <kaiquelopes.601@gmail.com>
2025-11-24 20:38:18 -0300

    feat: add method in class ProntuarioRepository

--- src/main/java/com/example/apidentalclinic/repositories/ProntuarioRepository.java
+++ src/main/java/com/example/apidentalclinic/repositories/ProntuarioRepository.java
@@ -2,11 +2,15 @@ package com.example.apidentalclinic.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
+
+import com.example.apidentalclinic.models.Paciente;
 import com.example.apidentalclinic.models.Prontuario;
+
 
 public interface ProntuarioRepository extends JpaRepository<Prontuario, Integer> {
 
     Optional<Prontuario> findByPacienteIdUsuario(int idUsuario);
 
     Optional<Prontuario> findByPacienteCpf(String cpf);
+    Optional<Prontuario> findPaciente(Paciente paciente);
 }
